5b7c15478cdbd4f126caa7a3d40557867590d0e1
Snezhana Yakovkina <snezhanayakovkina@gmail.com>
2023-03-21 02:38:50 +0900

    teather's edits

--- fpyjs-homeworks-1/scripts.js
+++ fpyjs-homeworks-1/scripts.js
@@ -1,49 +1,36 @@
-while (true) {
+let input = document.querySelector(".number-field__input");
+let button = document.querySelector(".number-field__button");
+let check = document.querySelector(".result__check__out");
+let help = document.querySelector(".result__help__out");
+let count = document.querySelector(".result__count__out");
 
-  let numberToGuess = Math.floor(Math.random() * 1000);
-  console.log("Я загадал: ", numberToGuess);
-  const numberFromUser = prompt("Введите число (от 0 до 999)");
-  console.log("Вы ввели: ", numberFromUser);
+let item = 0;
+let randNum = Math.floor(Math.random() * 1000);
+console.log("Я загадал: ", randNum);
 
-  const N = 1000;
-  const sortedArray = Array.from({ length: N }, (_, index) => index);
+button.onclick = function (evt) {
+  evt.presentDefault;
+
+  userNum = input.value;
   
-
-  function binarySearch(sortedArray, numberFromUser) {
-    let first = 0;
-    let last = sortedArray.length - 1;
-
-    while (first <= last) {
-      let middle = Math.floor((first + last) / 2);
-
-      if (numberFromUser === "q") {
-        alert("До свидания!");
-        break;
-      }
-      
-      if (isNaN(numberFromUser) || (parseInt(numberFromUser) !== 0 && parseInt(numberFromUser) !== 999)) {
-        alert("Вы ввели не числа из диапазона от 0 или 999");
-      } else 
-      if (sortedArray[middle] === numberFromUser) {
-        alert("Вы угадали!");        
-      } else if (sortedArray[middle] < numberFromUser) {
-        first = middle + 1;
-        alert("Загаданное число меньше");
-      } else if (sortedArray[middle] > numberFromUser) {
-        last = middle - 1;
-        alert("Загаданное число больше");
-      } 
-    }
-    return "Вы не угадали";
+  if (isNaN(userNum) || (parseInt(userNum) < 0 && parseInt(userNum) > 999)) {
+    help.textContent = "Вы ввели число не из диапазона от 0 или 999";
+    item++;
+    count.textContent = item;
+  } else if (userNum > randNum) {
+    check.textContent = "Вы не угадали";
+    help.textContent = "Много";
+    item++;
+    count.textContent = item;
+  } else if (userNum < randNum) {
+    check.textContent = "Вы не угадали";
+    help.textContent = "Мало";
+    item++;
+    count.textContent = item;
+  } else {
+    check.textContent = "Поздравляю! Вы угадали!";
+    help.textContent = "В самый раз";
+    item++;
+    count.textContent = item;
   }
-}
-
-
-
-
-
-
-
-
-
-
+};
